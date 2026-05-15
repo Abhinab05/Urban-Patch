bd9a11619646ba5395f0198f46c95622d77a8dd5
Abhinab05 <abhinabborborah13@gmail.com>
2026-05-15 20:02:09 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// =========================================
+// Project File - Auto Commented
+// These comments explain what the code does
+// =========================================
+// Importing required libraries/components
+import { defineConfig } from "vite";
+// Importing required libraries/components
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+});
